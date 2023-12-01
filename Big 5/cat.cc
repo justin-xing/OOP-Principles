41a1330fcd3a68f2a1bfb79fad07d19245763d68
Justin Xing <justinxing3@gmail.com>
2023-12-01 00:27:23 -0500

    implement big 5

---
+++ Big 5/cat.cc
@@ -0,0 +1,48 @@
+#include "cat.h"
+#include <utility>
+using namespace std;
+
+Cat::Cat(string name, string colour) : name{name}, colour{colour}, toyArray{new string [5]}, length{0} {}
+
+Cat::Cat(Cat &other) : name{other.name}, colour{other.name}, toyArray{new string [5]}, length(other.length) {
+    for (int i = 0; i < other.length; i++) {
+        toyArray[i] = other.toyArray[i];
+    }
+}
+
+Cat::Cat(Cat &&other) : name{other.name}, colour{other.colour}, toyArray{other.toyArray}, length(other.length) {
+    other.toyArray = nullptr;
+}
+
+Cat & operator=(Cat &other) {
+    if (*this == other) {
+        return *this;
+    } else {
+        name = other.name;
+        colour = other.colour;
+        length = other.length;
+        delete [] toyArray;
+        toyArray = new string [5];
+        for (int i = 0; i < length; i++) {
+            toyArray[i] = other.toyArray[i];
+        }
+        return *this;
+    }
+}
+
+Cat & operator=(Cat &&other) {
+    swap(name, other.name);
+    swap(colour, other.colour);
+    swap(length, other.length);
+    swap(toyArray, other.toyArray);
+    return *this;
+}
+
+Cat::~Cat() {
+    delete [] toyArray;
+}
+
+void Cat::giveToy(string toy) {
+    toyArray[length] = toy;
+    length++;
+}
---
+++ Big 5/cat.h
@@ -0,0 +1,23 @@
+#ifndef CAT_H
+#define CAT_H
+
+#include <string>
+
+struct Cat {
+    std::string name;
+    std::string colour;
+    std::string * toyArray;
+    int length;
+    
+    Cat(std::string name, std::string colour);
+    Cat(const Cat &other);
+    Cat(Cat &&other);
+    Cat &operator=(const Cat &other);
+    Cat &operator=(Cat &&other);
+    ~Cat();
+
+    void giveToy(std::string toy);
+
+}
+
+#endif
